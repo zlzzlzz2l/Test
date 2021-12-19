706a560fd0cb490a0b9237a2410ceec5eedd18e8
Ahyun Kim <zlzzlzz2l@naver.com>
2021-12-19 17:25:49 +0900

    Update: 람다식 클래스의 멤버 사용

--- lambda/src/MyFunctionalInterface.java
+++ lambda/src/MyFunctionalInterface.java
@@ -1,5 +1,6 @@
 @FunctionalInterface
 public interface MyFunctionalInterface {
-    public int method(int x, int y); // 리턴값이 있는 추상 메서드
+    // public int method(int x, int y); // 리턴값이 있는 추상 메서드
+    public void method();
     // public void otherMethod(); 컴파일 오류
 }
