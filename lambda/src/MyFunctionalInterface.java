3e748edbc9c5eb175775e2be102c1a7e12fe8928
Ahyun Kim <48669011+zlzzlzz2l@users.noreply.github.com>
2022-05-06 10:19:19 +0900

    Delete lambda/src directory

--- lambda/src/MyFunctionalInterface.java
+++
@@ -1,6 +0,0 @@
-@FunctionalInterface
-public interface MyFunctionalInterface {
-    // public int method(int x, int y); // 리턴값이 있는 추상 메서드
-    public void method();
-    // public void otherMethod(); 컴파일 오류
-}
