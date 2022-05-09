6da9633bbaac5e30b052f50ec71e67260d8b6b9a
Ahyun Kim <zlzzlzz2l@naver.com>
2022-05-09 23:07:09 +0900

    Refactor: Reflection Person 필요없는 메서드 삭제

--- src/main/java/com/example/reflection/Person.java
+++ src/main/java/com/example/reflection/Person.java
@@ -4,16 +4,8 @@ public class Person {
     private String name;
     private int age;
 
-    public String getName() {
-        return name;
-    }
-
     public void setName(String name) {
         this.name = name;
-    }
-
-    public int getAge() {
-        return age;
     }
 
     public void setAge(int age) {
